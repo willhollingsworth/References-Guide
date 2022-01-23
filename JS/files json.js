d27d7571181036644c53965fbc46bdef9aa2ca46
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-01-23 16:04:48 +1030

    moving js strings docs to multiple files

--- JS/files json.js
+++ JS/files json.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const file_path = path.join(__dirname, 'hello.json');
 
-const file = fs.readFileSync(file_path, 'utf8', (err, data) => {
+const await file = fs.readFile(file_path, 'utf8', (err, data) => {
     return data;
 });
 
