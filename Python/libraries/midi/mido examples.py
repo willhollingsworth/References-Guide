efb426811ded9fb420badba8c4cc9e7bdbc974a9
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-08-11 16:11:08 +0930

    better tests for launchpad

--- Python/libraries/midi/mido examples.py
+++ Python/libraries/midi/mido examples.py
@@ -12,13 +12,13 @@ def monitor_midi():
     time.sleep(.1)
 
 
-def blink_lights(loops=4, volocity_limit=2, msgs=['note_on', 'note_off'], channel_limit=15, note_limit=127, sleep=.5):
+def blink_lights(loops=4, velocity_limit=2, msgs=['note_on', 'note_off'], channel_limit=15, note_limit=127, sleep=.5, channel_start=0):
     # will loop over a large number of settings quickly
     # useful to find how a controller reacts to midi signals
     for loop_count in range(loops):
-        for velocity_val in range(volocity_limit):
+        for velocity_val in range(velocity_limit):
             for msg in msgs:
-                for channel_val in range(channel_limit):
+                for channel_val in range(channel_start, channel_limit):
                     for note_val in range(note_limit):
                         out_port.send(mido.Message(
                             msg, channel=channel_val, note=note_val, velocity=velocity_val))
@@ -65,8 +65,10 @@ def print_midi_devices():
 
 
 print_midi_devices()
-in_port, out_port = define_in_out_midi_ports('MIDI Mix')
-scroll_lights()
+# in_port, out_port = define_in_out_midi_ports('MIDI Mix')
+in_port, out_port = define_in_out_midi_ports('Launchpad')
+# scroll_lights()
 
-# blink_lights()
+blink_lights(channel_start=0, channel_limit=1,
+             velocity_limit=127, msgs=['note_on'], note_limit=90)  # good test for the launchpad mk2
 # monitor_midi()
