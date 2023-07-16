3c7739632f5a5971475696feae24e86db3e0818d
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2023-07-16 15:34:36 +0930

    cli interface example

---
+++ Python/command_line/cli_interface.py
@@ -0,0 +1,49 @@
+import os
+
+def clear_screen():
+    os.system('cls')
+
+def invalid_choice():
+    print('invalid choice, please re-pick')
+    input()
+    show_menu()
+
+def show_menu():
+    clear_screen()
+    options = [1,2,3]
+    menu = '''
+    1. Option A
+    2. Option B
+    3. Exit
+    '''
+    print(menu)
+    choice = input(" >>  ")
+    try : choice = int(choice)
+    except : invalid_choice()
+    if choice in options:
+        clear_screen()
+        if choice == 1 : option_1()
+        elif choice == 2 : option_2()
+        elif choice == 3 : exit()
+    else : invalid_choice()
+
+def option_1():
+    option_1 = '''
+    welcome to option 1
+    press any key to return to main menu
+    '''
+    print(option_1)
+    input()
+    show_menu()
+
+def option_2():
+    option_2 = '''
+    welcome to option 1
+    press any key to return to main menu
+    '''
+    print(option_2)
+    input()
+    show_menu()
+
+if __name__ == '__main__':
+    show_menu()
