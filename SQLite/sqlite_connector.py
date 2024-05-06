23889b79c332ff18249162f11146c37bd060888f
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2024-05-06 23:17:32 +0930

    adding pandas output to sqlite docs

--- SQLite/sqlite_connector.py
+++ SQLite/sqlite_connector.py
@@ -5,21 +5,38 @@ https://datagy.io/python-sqlite-tutorial/
 '''
 import sqlite3
 from pathlib import Path
+import pandas as pd
+from IPython.display import HTML
 
 home_path = str(Path.home())
 db_path_relative = r'\AppData\Roaming\DBeaverData\workspace6\.metadata\sample-database-sqlite-1\Chinook.db'
 db_path =  home_path + db_path_relative
 
-def sql_print_command(command):
-    connection = sqlite3.connect(db_path)
+connection = sqlite3.connect(db_path)
+
+def sql_run_query(query):
     cursor = connection.cursor()
-    result = cursor.execute(command)
-    for row in result.fetchall():
-        parse_row = [str(item) for item in row]
-        print(' '.join(parse_row))             
+    result = cursor.execute(query).fetchall()
     cursor.close()
+    return result
+
+def sql_print_command(query):
+    result = sql_run_query(query)
+    for row in result:
+        parse_row = [str(item) for item in row]
+        print(' '.join(parse_row))   
+
+def sql_return_df(query):
+    df = pd.read_sql_query(query,connection)
+    return df
+
+def sql_return_html(query):
+    df = sql_return_df(query)
+    html = HTML(df.to_html(index=False))
+    return html
 
 if __name__ == '__main__':
     command = "SELECT * FROM Genre LIMIT 3"
-    sql_print_command(command)               
-          
+    # sql_print_command(command)               
+    # sql_return_df(command)
+    print(sql_return_df(command))
